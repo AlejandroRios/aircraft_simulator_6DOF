99509cb933219e52b2167f4a68baf0c058b90091
Alejandro Rios <aarc.88@gmail.com>
2024-12-08 23:18:30 -0300

    images net

--- Dat/Nova pasta/teste_run_ANN.py
+++ Dat/Nova pasta/teste_run_ANN.py
@@ -1,13 +1,17 @@
+import joblib
 import torch
-import torch.nn as nn
 import numpy as np
+
+# Carregar os scalers
+scaler_X = joblib.load("scaler_X.pkl")
+scaler_y = joblib.load("scaler_y.pkl")
 
 # Carregar o modelo TorchScript
 scripted_model = torch.jit.load("model_scripted.pt")
 scripted_model.eval()  # Certifique-se de que está em modo de avaliação
 
 # Preparar os dados de entrada
-input_data = np.array([[5.0, 0.0, 0.8, 1000, 0.0, 0.0, 0.0, 0.0]])  # Exemplo de entrada
+input_data = np.array([[0.0, 0.0, 0.8, 1000, 0.0, 0.0, 0.0, 0.0]])  # Exemplo de entrada
 input_normalized = scaler_X.transform(input_data)  # Normalizar os dados
 
 # Fazer a previsão
